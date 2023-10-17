9ab16892e45836bed36e4f10c5a23b2eede2c6aa
“Juan <ramirezjuanj0117@gmail.com>
2023-10-17 17:11:09 -0500

    Frontend setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App ()
+{
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
